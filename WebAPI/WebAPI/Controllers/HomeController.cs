208ccc3ed6c6197e40cd3273ecee87ecce939657
leonid.mashlyakevich <leonid.mashlyakevich@gmail.com>
2022-10-26 21:35:07 +0300

    WebAPI with other feature

---
+++ WebAPI/WebAPI/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    [ApiController]
+    [Route("home")]
+    public class HomeController : ControllerBase
+    {
+        private readonly IHomeSer _iHomeSer;
+        private readonly IConfiguration _config;
+
+        public HomeController(IHomeSer iHomeSer, IConfiguration config)
+        {
+            _iHomeSer = iHomeSer;
+            _config = config;
+        }
+        [HttpGet("home123")]
+        public string Get()
+        {
+            return "";
+        }
+        [HttpGet("say")]
+        public string Say()
+        {
+           return _iHomeSer.Say();
+        }
+        [HttpGet("secret-key")]
+        public string GettKeyy()
+        {
+            return _config.GetValue<string>("Sekrtr");
+        }
+    }
+}
---
+++ WebAPI/WebAPI/Controllers/PersonController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    [Route("[controller]")]
+    public class PersonController : ControllerBase
+    {
+       
+
+        [HttpGet] 
+        public List<Person> Get()
+        {
+            return UI.Start();
+            
+        }
+       
+
+    }
+}
