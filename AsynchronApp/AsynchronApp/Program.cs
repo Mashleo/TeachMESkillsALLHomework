11d2ff8fc8b742266bd23e3115054060f917f98f
leonid.mashlyakevich <leonid.mashlyakevich@gmail.com>
2022-11-14 19:22:31 +0300

    simple Asynchronous and Synchronous application

---
+++ AsynchronApp/AsynchronApp/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsynchronApp
+{
+
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Apartment renovation");
+            CreateDesignProject();
+
+            var furnishings = Furnishings();
+            var floorRepair = FloorRepair();
+            var wallRepair = WallRepair();
+            
+            CeilingRepair();
+
+            await floorRepair;
+            await furnishings;
+            await wallRepair;
+
+        }
+        static void CreateDesignProject()
+        {
+            Console.WriteLine("Create gesign project");
+        }
+
+        static async Task FloorRepair()
+        {
+            Console.WriteLine("Removing old wood floor");
+            Console.WriteLine("Pour floor screed and wait for it to dry");
+            await Task.Delay(5000);
+            Console.WriteLine("Lay laminate");
+        }
+
+        static async Task WallRepair()
+        {
+            Console.WriteLine("Removing old wallpaper");
+            Console.WriteLine("Wall plaster");
+            await Task.Delay(2000);
+            Console.WriteLine("Put up new wallpaper");
+            await Task.Delay(2000);
+
+        }
+
+        static void CeilingRepair()
+        {
+            Console.WriteLine("Removing old ceiling wallpaper");
+            Console.WriteLine("Put up new stretch ceiling");
+
+        }
+
+        static async Task Furnishings()
+        {
+            Console.WriteLine("Buy furnishings");
+            await Task.Delay(6000);
+            Console.WriteLine("Put furnishings");
+        }
+    }
+}
+
