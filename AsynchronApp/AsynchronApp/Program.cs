e98166131ebd4dd4d71a500e91f5b51845a9dabe
leonid.mashlyakevich <leonid.mashlyakevich@gmail.com>
2022-11-14 21:46:19 +0300

    TASK 6

--- AsynchronApp/AsynchronApp/Program.cs
+++ AsynchronApp/AsynchronApp/Program.cs
@@ -9,56 +9,12 @@ namespace AsynchronApp
     {
         static async Task Main(string[] args)
         {
-            Console.WriteLine("Apartment renovation");
-            CreateDesignProject();
 
-            var furnishings = Furnishings();
-            var floorRepair = FloorRepair();
-            var wallRepair = WallRepair();
-            
-            CeilingRepair();
-
-            await floorRepair;
-            await furnishings;
-            await wallRepair;
-
-        }
-        static void CreateDesignProject()
-        {
-            Console.WriteLine("Create gesign project");
-        }
-
-        static async Task FloorRepair()
-        {
-            Console.WriteLine("Removing old wood floor");
-            Console.WriteLine("Pour floor screed and wait for it to dry");
-            await Task.Delay(5000);
-            Console.WriteLine("Lay laminate");
-        }
-
-        static async Task WallRepair()
-        {
-            Console.WriteLine("Removing old wallpaper");
-            Console.WriteLine("Wall plaster");
-            await Task.Delay(2000);
-            Console.WriteLine("Put up new wallpaper");
-            await Task.Delay(2000);
+            await StartRepair.Start();
 
         }
 
-        static void CeilingRepair()
-        {
-            Console.WriteLine("Removing old ceiling wallpaper");
-            Console.WriteLine("Put up new stretch ceiling");
-
-        }
-
-        static async Task Furnishings()
-        {
-            Console.WriteLine("Buy furnishings");
-            await Task.Delay(6000);
-            Console.WriteLine("Put furnishings");
-        }
     }
+
 }
 
